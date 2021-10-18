1f7cae2add2a40e439b6bd5039e316ee9413d0a0
Genadi <genadi.kozarev@gmail.com>
2021-10-18 16:01:34 +0100

    connecting doctor's name with the dropdown menu

--- client/src/Components/DoctorDropdownMenu/DoctorDropdownMenu.js
+++ client/src/Components/DoctorDropdownMenu/DoctorDropdownMenu.js
@@ -1,45 +1,28 @@
 import React, {useEffect, useState} from "react";
 
 const DoctorDropdownMenu = () => {
-
-    const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [items, setItems] = useState([]);
+    const [allDoctors, setAllDoctors] = useState([]);
+    const doctors = async (e) => {
+        const response = await fetch('http://localhost:5000/doctors')
+        let doctorsResponse = await response.json();
+        setAllDoctors(doctorsResponse)
+    }
 
     useEffect(() => {
-        fetch('http://localhost:5000/doctors').then((data) => {
-            return data.json()
-        }).then((data) => {
-            setItems(data);
-            console.log(items[0])
-
-        }, (error) => {
-            setIsLoaded(true);
-            setError(error);
-        })
+        doctors()
     }, [])
 
-    if (error) {
-        return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-        return <div>Loading...</div>;
-    } else {
-
-        return (
-
-            <div>
-                <select name={"doctors"} id={"doctors"}>
-                    <option value="doctor1" data-doctorId={"doctorId"}>{items[0].firstName}</option>
-                    <option value="doctor2" data-doctorId={"doctorId"}></option>
-                    <option value="doctor3" data-doctorId={"doctorId"}></option>
-                    <option value="doctor4" data-doctorId={"doctorId"}></option>
-                    <option value="doctor5" data-doctorId={"doctorId"}></option>
-                </select>
-            </div>
-
-
-        );
-    }
+    return (
+        <div>
+            <select name={"doctors"} id={"doctors"}>
+                {allDoctors.map((doctor) => {
+                    return (
+                        <option value="" data-doctorId={doctor._id}>Dr. {doctor.firstName + ' ' + doctor.lastName}</option>
+                    )
+                })}
+            </select>
+        </div>
+    );
 }
 
-    export default DoctorDropdownMenu
+export default DoctorDropdownMenu
