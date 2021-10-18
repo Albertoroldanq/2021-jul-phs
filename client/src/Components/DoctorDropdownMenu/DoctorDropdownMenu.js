515fc250d977260f40d19913e048bb7a39eee422
Freddie Reschwamm <fredreschwamm@outlook.com>
2021-10-18 15:08:30 +0100

    fetch fix

--- client/src/Components/DoctorDropdownMenu/DoctorDropdownMenu.js
+++ client/src/Components/DoctorDropdownMenu/DoctorDropdownMenu.js
@@ -1,58 +1,45 @@
 import React, {useEffect, useState} from "react";
 
 const DoctorDropdownMenu = () => {
-//     return (
-//         <div>
-//             <select name={"doctors"} id={"doctors"}>
-//                 <option value="doctor1" data-doctorId={"doctorId"}>Doctor1</option>
-//                 <option value="doctor2" data-doctorId={"doctorId"}></option>
-//                 <option value="doctor3" data-doctorId={"doctorId"}></option>
-//                 <option value="doctor4" data-doctorId={"doctorId"}></option>
-//                 <option value="doctor5" data-doctorId={"doctorId"}></option>
-//             </select>
-//         </div>
-//     )
-// }
-        const [error, setError] = useState(null);
-        const [isLoaded, setIsLoaded] = useState(false);
-        const [items, setItems] = useState([]);
 
-        // Note: the empty deps array [] means
-        // this useEffect will run once
-        // similar to componentDidMount()
-        useEffect(() => {
-            fetch("http://localhost:5000/doctors")
-                .then(res => res.json())
-                .then(
-                    (result) => {
-                        setIsLoaded(true);
-                        setItems(result);
-                    },
-                    // Note: it's important to handle errors here
-                    // instead of a catch() block so that we don't swallow
-                    // exceptions from actual bugs in components.
-                    (error) => {
-                        setIsLoaded(true);
-                        setError(error);
-                    }
-                )
-        }, [])
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
 
-        if (error) {
-            return <div>Error: {error.message}</div>;
-        } else if (!isLoaded) {
-            return <div>Loading...</div>;
-        } else {
-            return (
-                <ul>
-                    {items.map(item => (
-                        <li key={item.id}>
-                            {item.name}
-                        </li>
-                    ))}
-                </ul>
-            );
-        }
+    useEffect(() => {
+        fetch('http://localhost:5000/doctors').then((data) => {
+            return data.json()
+        }).then((data) => {
+            setItems(data);
+            console.log(items[0])
+
+        }, (error) => {
+            setIsLoaded(true);
+            setError(error);
+        })
+    }, [])
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Loading...</div>;
+    } else {
+
+        return (
+
+            <div>
+                <select name={"doctors"} id={"doctors"}>
+                    <option value="doctor1" data-doctorId={"doctorId"}>{items[0].firstName}</option>
+                    <option value="doctor2" data-doctorId={"doctorId"}></option>
+                    <option value="doctor3" data-doctorId={"doctorId"}></option>
+                    <option value="doctor4" data-doctorId={"doctorId"}></option>
+                    <option value="doctor5" data-doctorId={"doctorId"}></option>
+                </select>
+            </div>
+
+
+        );
     }
+}
 
-export default DoctorDropdownMenu
+    export default DoctorDropdownMenu
