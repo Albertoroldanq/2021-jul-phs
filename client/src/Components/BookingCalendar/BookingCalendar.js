c3eb13edf327d42af2ad31dc7345dc7fcabd462d
Alberto <albertoroldanq@gmail.com>
2021-10-20 14:04:11 +0100

    working on displaying appointments

--- client/src/Components/BookingCalendar/BookingCalendar.js
+++ client/src/Components/BookingCalendar/BookingCalendar.js
@@ -5,7 +5,7 @@ import 'react-calendar/dist/Calendar.css';
 const BookingCalendar = (props) => {
 
     const [value, onChange] = useState(new Date())
-    const [availableAppointments, setAvailableAppointments] = useState('')
+    const [availableAppointments, setAvailableAppointments] = useState(JSON.parse([{"date":["Wed", "Oct", 20, 2021], "time":"11", "doctorId": "616dd078a8a7900545ee891a"}]))
 
     const getAppointments = async (date) => {
         const response = await fetch(`http://localhost:5000/appointments/${props.currentDoctor._id}/${date}`)
@@ -21,14 +21,14 @@ const BookingCalendar = (props) => {
                 value={value}
                 onClickDay={getAppointments(value.toDateString())}
             />
-            {props.currentDoctor.timeSlots ?
-                availableAppointments.time.map((slot) => {
-                    return (
-                        <p>{slot + " - " + parseInt(slot + 1)}</p>
-                    )
-                })}
-        </div>
-    );
-}
 
+
+            {availableAppointments.time.map((slot) => {
+                return (
+                    <p>{slot + " - " + parseInt(slot + 1)}</p>
+                )
+            })}
+        </div>
+    )
+}
 export default BookingCalendar
