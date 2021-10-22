13b274b5bb5435e773c0aca35d667cd16a94e6ca
Alberto <albertoroldanq@gmail.com>
2021-10-22 11:36:08 +0100

    displaying only first name of patient

--- client/src/Components/AppointmentBooked/AppointmentBooked.js
+++ client/src/Components/AppointmentBooked/AppointmentBooked.js
@@ -3,7 +3,9 @@ import {useLocation, Link} from "react-router-dom";
 
 const AppointmentBooked = () => {
     const search = useLocation().search;
-    const name = new URLSearchParams(search).get('name')
+    const fullName = new URLSearchParams(search).get('name')
+    const name = fullName.split(' ')
+    const firstName = name[0]
     const description = new URLSearchParams(search).get('description')
     const day = new URLSearchParams(search).get('day')
     const month = new URLSearchParams(search).get('month')
@@ -18,7 +20,7 @@ const AppointmentBooked = () => {
                 <h2>Successfully Booked!</h2>
             </div>
             <div>
-                <p>Hi {name} your appointment has been booked with Dr. {doctorLastName} at {time}h on {day}/{month}/{year}</p>
+                <p>Hi {firstName} your appointment has been booked with Dr. {doctorLastName} at {time}h on {day}/{month}/{year}</p>
                 <p>These are the appointment details provided by you:</p>
                 <p>{description}</p>
             </div>
