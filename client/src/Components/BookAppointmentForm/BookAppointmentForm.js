284af80b2cb0c1f4a8cc3231996dc242bad43642
Alberto <albertoroldanq@gmail.com>
2021-10-25 19:34:53 +0100

    created BookingAppointmentForm component

---
+++ client/src/Components/BookAppointmentForm/BookAppointmentForm.js
@@ -0,0 +1,105 @@
+import React, {useEffect, useState} from 'react';
+import {Link} from 'react-router-dom'
+
+const BookAppointmentForm = (props) => {
+    const [patientName, setPatientName] = useState(null)
+    const [patientEmail, setPatientEmail] = useState(null)
+    const [appointmentDescription, setAppointmentDescription] = useState(null)
+    const [bookedSuccessLink, setBookedSuccessLink] = useState('')
+    const [bookButtonState, setBookButtonState] = useState('disabled')
+    const [dataValidationErrorMessage, setDataValidationErrorMessage] = useState('hideDataValidationErrorMessage')
+
+    const validateEmail = (mail) => {
+        const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        if(mail !== null) {
+            if (mail.match(mailformat)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    const handleEmail = (value) => {
+        if (!validateEmail(value)) {
+            setDataValidationErrorMessage('showDataValidationErrorMessage')
+        } else {
+            setDataValidationErrorMessage('hideDataValidationErrorMessage')
+        }
+    }
+
+    const handleSubmit = async () => {
+        if ((props.appointmentTime !== null || '') && (props.patientName !== null || '') && (props.patientEmail !== null) && (props.appointmentDescription !== null || '')) {
+            const rawResponse = await fetch('http://localhost:5000/appointmentBooked/', {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    "date": props.date,
+                    "doctorId": props.currentDoctor._id,
+                    "time": props.appointmentTime,
+                    "name": props.patientName,
+                    "email": props.patientEmail,
+                    "description": props.appointmentDescription
+                })
+            })
+        }
+    }
+
+    useEffect(() => {
+        fetch('http://localhost:5000/doctors/' + props.currentDoctor._id + '/' + props.date).then(
+            response => response.json().then(response => {
+                props.setBookedAppointments(response)
+            })
+        )
+
+        if ((props.appointmentTime !== null || '') && (patientName !== null || '') && (patientEmail !== null || '') && (validateEmail(patientEmail)) && (appointmentDescription !== null || '')) {
+            setBookedSuccessLink(`/appointmentBooked?description=${appointmentDescription}&day=${props.day}&month=${props.month}&year=${props.year}&doctorLastName=${props.currentDoctor.lastName}&time=${props.appointmentTime}&name=${patientName}`)
+            setBookButtonState('enabled')
+        } else if ((props.appointmentTime === null || '') || (patientName === null || '') || (patientEmail === null) || (appointmentDescription === null || '')) {
+            setBookedSuccessLink('/')
+            setBookButtonState('disabled')
+        }
+
+        if (!validateEmail(patientEmail)) {
+            setDataValidationErrorMessage('showDataValidationErrorMessage')
+        } else {
+            validateEmail('hideDataValidationErrorMessage')
+        }
+    }, [props.currentDoctor, props.value, patientName, appointmentDescription, patientEmail, props.appointmentTime])
+
+    return (
+        <div className={"patientDetailsForm"}>
+            <h2>3. Introduce your details</h2>
+            <div className={"bookingForm"}>
+                <input type="hidden" value={props.currentDoctor._id} name="doctor"/>
+                <input type="hidden" value={props.date} name="date"/>
+                <input type="hidden" value={props.appointmentTime} name="time"/>
+                <label>Full name</label>
+                <input type="text" required placeholder="Enter your name" name="name"
+                       onChange={e => setPatientName(e.target.value)} className="textInput"/>
+                <label>Email</label>
+                <input type="email" required placeholder="Enter your email" name="email"
+                       onChange={e => {
+                           setPatientEmail(e.target.value)
+                           handleEmail(e.target.value)
+                       }} className="textInput"/>
+                <label>Describe symptoms</label>
+                <textarea className="textareaInput" type="textarea" rows="5" cols="1" placeholder="Tell us more"
+                          name="description" onChange={e => setAppointmentDescription(e.target.value)}
+                          required/>
+                <div className="dataValidationErrorMessage">
+                    <p className={dataValidationErrorMessage}>Please, enter a valid email address.</p>
+                </div>
+                <div>
+                    <Link to={bookedSuccessLink} value="Book an appointment!" onClick={handleSubmit}
+                          className={bookButtonState + " bookButton"}>Book an appointment</Link>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default BookAppointmentForm
--- client/src/Components/BookingCalendar/BookingCalendar.js
+++ client/src/Components/BookingCalendar/BookingCalendar.js
@@ -1,11 +1,12 @@
 import React, {useEffect, useState} from 'react';
 import Calendar from 'react-calendar';
-import {Link} from 'react-router-dom'
 import 'react-calendar/dist/Calendar.css';
 import './BookingCalendar.css'
 
 import '../TimeSlots/TimeSlots'
+import '../BookAppointmentForm/BookAppointmentForm'
 import TimeSlots from "../TimeSlots/TimeSlots";
+import BookAppointmentForm from "../BookAppointmentForm/BookAppointmentForm";
 
 const BookingCalendar = (props) => {
     const today = new Date()
@@ -18,84 +19,12 @@ const BookingCalendar = (props) => {
     const [minDate, setMinDate] = useState(tomorrow)
     const [bookedAppointments, setBookedAppointments] = useState([])
     const [appointmentTime, setAppointmentTime] = useState(null)
-    const [patientName, setPatientName] = useState(null)
-    const [patientEmail, setPatientEmail] = useState(null)
-    const [appointmentDescription, setAppointmentDescription] = useState(null)
     const [displaytimeSlotsAndDates, setVisibility] = useState('hidden')
-    const [bookedSuccessLink, setBookedSuccessLink] = useState('')
-    const [bookButtonState, setBookButtonState] = useState('disabled')
-    const [timeSlotSelected, setTimeSlotSelected] = useState(null)
-    const [dataValidationErrorMessage, setDataValidationErrorMessage] = useState('hideDataValidationErrorMessage')
+    const [day, setDay] = useState(value.getDate().toString())
+    const [month, setMonth] = useState((value.getMonth() + 1).toString())
+    const [year, setYear] = useState(value.getFullYear().toString())
+    const [date, setDate] = useState(day + month + year)
 
-    const day = value.getDate().toString()
-    const month = (value.getMonth() + 1).toString()
-    const year = value.getFullYear().toString()
-    const date = day + month + year
-
-    const validateEmail = (mail) => {
-        const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-        if(mail !== null) {
-            if (mail.match(mailformat)) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-    }
-
-    const handleEmail = (value) => {
-        if (!validateEmail(patientEmail)) {
-            setDataValidationErrorMessage('showDataValidationErrorMessage')
-        } else {
-            setDataValidationErrorMessage('hideDataValidationErrorMessage')
-        }
-    }
-
-    useEffect(() => {
-        fetch('http://localhost:5000/doctors/' + props.currentDoctor._id + '/' + date).then(
-            response => response.json().then(response => {
-                setBookedAppointments(response)
-                setVisibility('')
-            })
-        )
-
-        if ((appointmentTime !== null || '') && (patientName !== null || '') && (patientEmail !== null || '') && (validateEmail(patientEmail)) && (appointmentDescription !== null || '')) {
-            setBookedSuccessLink(`/appointmentBooked?description=${appointmentDescription}&day=${day}&month=${month}&year=${year}&doctorLastName=${props.currentDoctor.lastName}&time=${appointmentTime}&name=${patientName}`)
-            setBookButtonState('enabled')
-        } else if ((appointmentTime === null || '') || (patientName === null || '') || (patientEmail === null) || (appointmentDescription === null || '')) {
-               setBookedSuccessLink('/')
-               setBookButtonState('disabled')
-        }
-        if (!validateEmail(patientEmail)) {
-            setDataValidationErrorMessage('showDataValidationErrorMessage')
-        } else {
-            setDataValidationErrorMessage('hideDataValidationErrorMessage')
-        }
-
-    }, [props.currentDoctor, value, patientName, appointmentDescription, patientEmail, appointmentTime])
-
-
-    const handleSubmit = async () => {
-        if ((appointmentTime !== null || '') && (patientName !== null || '') && (patientEmail !== null) && (appointmentDescription !== null || '')) {
-
-            const rawResponse = await fetch('http://localhost:5000/appointmentBooked/', {
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    "date": date,
-                    "doctorId": props.currentDoctor._id,
-                    "time": appointmentTime,
-                    "name": patientName,
-                    "email": patientEmail,
-                    "description": appointmentDescription
-                })
-            })
-
-        }
-    }
 
     return (
         value.getDay() !== 6 && value.getDay() !== 0 ?
@@ -106,33 +35,9 @@ const BookingCalendar = (props) => {
                               next2Label={null} prev2Label={null} maxDate={maxDate}/>
                     <TimeSlots appointmentTime={appointmentTime} setAppointmentTime={setAppointmentTime} bookedAppointments={bookedAppointments} />
                 </div>
-                <div className={"patientDetailsForm"}>
-                    <h2>3. Introduce your details</h2>
-                    <div className={"bookingForm"}>
-                        <input type="hidden" value={props.currentDoctor._id} name="doctor"/>
-                        <input type="hidden" value={date} name="date"/>
-                        <input type="hidden" value={appointmentTime} name="time"/>
-                        <label>Full name</label>
-                        <input type="text" required placeholder="Enter your name" name="name"
-                               onChange={e => setPatientName(e.target.value)} className="textInput"/>
-                        <label>Email</label>
-                        <input type="email" required placeholder="Enter your email" name="email"
-                               onChange={e => {
-                                   setPatientEmail(e.target.value)
-                                   handleEmail(e.target.value)}} className="textInput"/>
-                        <label>Describe symptoms</label>
-                        <textarea className="textareaInput" type="textarea" rows="5" cols="1" placeholder="Tell us more"
-                                  name="description" onChange={e => setAppointmentDescription(e.target.value)}
-                                  required/>
-                        <div className="dataValidationErrorMessage">
-                            <p className={dataValidationErrorMessage}>Please, enter a valid email address.</p>
-                        </div>
-                        <div>
-                            <Link to={bookedSuccessLink} value="Book an appointment!" onClick={handleSubmit}
-                                  className={bookButtonState + " bookButton"}>Book an appointment</Link>
-                        </div>
-                    </div>
-                </div>
+                <BookAppointmentForm appontmentTime={appointmentTime} currentDoctor={props.currentDoctor}
+                                     date={date} setBookedAppointments={setBookedAppointments} appointmentTime={appointmentTime}
+                                     day={day} month={month} year={year} date={date} value={value}/>
             </div>
             :
             <div id="availabilityContainer" className={displaytimeSlotsAndDates}>
